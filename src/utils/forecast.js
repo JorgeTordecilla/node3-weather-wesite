9d23b1b995f5f7bd49b2b9e9bd989bc5dc71f6db
JorgeTordecilla <jtordeci@ibero.edu.co>
2021-09-14 16:37:12 -0500

    add more info to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,13 +2,14 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=41db79f8a89299dfaad8474b4e12be27&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
+    console.log(url)
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ", It is currently " + body.current.temperature + " degrees out. It is feels like " + body.current.feelslike + " degrees out.")
+            callback(undefined, body.current.weather_descriptions[0] + ", It is currently " + body.current.temperature + " degrees out. It is feels like " + body.current.feelslike + " degrees out. The humidity percent is:  " + body.current.humidity + "%.")
         }
     })
 }
